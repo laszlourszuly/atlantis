6cde5270e83dec8d1e0faa5b97510b6c10b28e21
László Urszuly <echsylon@gmail.com>
2016-10-03 23:11:01 +0200

    Enables Strict Mode in sample application

--- sample/src/main/java/com/echsylon/sample/SampleApplication.java
+++ sample/src/main/java/com/echsylon/sample/SampleApplication.java
@@ -1,6 +1,21 @@
 package com.echsylon.sample;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 public class SampleApplication extends Application {
+    @Override
+    public void onCreate() {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .build());
+
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .penaltyDeath()
+                .build());
+        super.onCreate();
+    }
 }
