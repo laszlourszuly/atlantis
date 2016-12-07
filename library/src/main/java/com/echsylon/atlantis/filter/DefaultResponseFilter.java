465ab5b601e962bac441e112e3968fc2687675c5
László Urszuly <laszlo.urszuly@jayway.com>
2016-12-07 13:40:30 +0100

    Implements DefaultResponseFilter

--- library/src/main/java/com/echsylon/atlantis/filter/DefaultResponseFilter.java
+++ library/src/main/java/com/echsylon/atlantis/filter/DefaultResponseFilter.java
@@ -6,8 +6,17 @@ import java.util.List;
 
 public class DefaultResponseFilter implements MockResponse.Filter {
 
+    /**
+     * Returns the first mock response in the given list.
+     *
+     * @param mockResponses All available mockResponse to pick a candidate from.
+     *                      Null and empty lists are handled gracefully.
+     * @return The first mock response or null if the list is null or empty.
+     */
     @Override
     public MockResponse findResponse(List<MockResponse> mockResponses) {
-        return null;
+        return mockResponses != null && !mockResponses.isEmpty() ?
+                mockResponses.get(0) :
+                null;
     }
 }
