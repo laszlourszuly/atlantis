4959f543bda522346b90340057f7c078a45ec6d1
László Urszuly <urszuly.laszlo@gmail.com>
2021-11-22 09:43:34 +0100

    Add demo app

---
+++ demo/app/src/main/java/com/echsylon/atlantis/demo/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.echsylon.atlantis.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.lifecycle.lifecycleScope
+import com.echsylon.atlantis.Atlantis
+import com.echsylon.atlantis.demo.databinding.MainActivityBinding
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { MainActivityBinding.inflate(LayoutInflater.from(this)) }
+    private val httpClient by lazy { HttpClient.create() }
+    private val atlantis by lazy { Atlantis() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val configStream = assets.open("atlantis_config.json")
+        atlantis.addConfiguration(configStream)
+        atlantis.start(8080)
+
+        binding.actionJson.setOnClickListener { onJsonClick() }
+        binding.actionSerial.setOnClickListener { onNextClick() }
+        binding.actionRandom.setOnClickListener { onRandomClick() }
+    }
+
+    private fun onJsonClick() {
+        binding.responseJson.text = null
+        binding.actionJson.isEnabled = false
+        lifecycleScope.launch(IO) {
+            httpClient
+                .runCatching { getJsonString() }
+                .onFailure { it.printStackTrace() }
+                .onSuccess { withContext(Main) { binding.responseJson.text = it } }
+                .also { withContext(Main) { binding.actionJson.isEnabled = true } }
+        }
+    }
+
+    private fun onNextClick() {
+        binding.responseSerial.text = null
+        binding.actionSerial.isEnabled = false
+        lifecycleScope.launch(IO) {
+            httpClient
+                .runCatching { getNextString() }
+                .onFailure { it.printStackTrace() }
+                .onSuccess { withContext(Main) { binding.responseSerial.text = it } }
+                .also { withContext(Main) { binding.actionSerial.isEnabled = true } }
+        }
+    }
+
+    private fun onRandomClick() {
+        binding.responseRandom.text = null
+        binding.actionRandom.isEnabled = false
+        lifecycleScope.launch(IO) {
+            httpClient
+                .runCatching { getAnyString() }
+                .onFailure { it.printStackTrace() }
+                .onSuccess { withContext(Main) { binding.responseRandom.text = it } }
+                .also { withContext(Main) { binding.actionRandom.isEnabled = true } }
+        }
+    }
+}
